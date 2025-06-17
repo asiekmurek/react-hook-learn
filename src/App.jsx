52d54d183ca9b371ca49e24e12706ca98daa1ba7
Joanna Murawska <jkmurek@gmail.com>
2025-06-17 18:44:34 +0200

    useState - exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const myTags = [
+    { id: 1, value: "aaa"},
+    { id: 2, value: "bbb"},
+  ]
+
+  const addTag = () => {
+    let id = Math.random()
+    let value = Math.random().toString(36)
+    const newTag = {id: id, value: value}
+    setTag( [...tag, newTag])
+  }
+    
+    
+  const [tag, setTag] = useState(myTags)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     {tag.map( (item) => 
+      <div key={item.id}>
+        <li>{item.id}</li>
+        <li>{item.value}</li>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
+
+    )}
+     <button onClick={addTag}>Add Tag</button>
     </>
   )
 }
