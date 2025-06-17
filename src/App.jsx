9e59baaf285c23158d8403da2e00865c9de9b91c
Joanna Murawska <jkmurek@gmail.com>
2025-06-17 19:06:32 +0200

    useEffect exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
 
-  const myTags = [
-    { id: 1, value: "aaa"},
-    { id: 2, value: "bbb"},
-  ]
+const [count, setCount] = useState(0)
 
-  const addTag = () => {
-    let id = Math.random()
-    let value = Math.random().toString(36)
-    const newTag = {id: id, value: value}
-    setTag( [...tag, newTag])
-  }
-    
-    
-  const [tag, setTag] = useState(myTags)
+useEffect( () => {
+  console.log("Licznik zmienił się na: " + count)
+
+  return () => {
+    console.log("Komponent odmontowany")
+  };
+}, [count]);
+
+
+const handleCount = () => {
+  setCount(count + 1)
+}
 
   return (
     <>
-     {tag.map( (item) => 
-      <div key={item.id}>
-        <li>{item.id}</li>
-        <li>{item.value}</li>
-      </div>
-     
+      <span>{count}</span>
+      <button onClick={handleCount} >Zwiększ</button>
 
-    )}
-     <button onClick={addTag}>Add Tag</button>
     </>
   )
 }
